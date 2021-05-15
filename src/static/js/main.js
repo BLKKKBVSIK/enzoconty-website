9919a1ea30ed026be1e361c7042c7cfe53fe6166
Enzo CONTY <indefined3@gmail.com>
2021-05-15 14:34:02 +0200

    :bento: Adding statics assets / stylesheets etc..

---
+++ src/static/js/main.js
@@ -0,0 +1,49 @@
+let lastQuickview = "nil";
+
+function quickviewlang(quickviewName, quickviews) {
+    if ($('div#' + quickviewName + '.is-active').length) {
+        $('div#' + quickviewName + '.is-active').removeClass('is-active');
+        //document.querySelectorAll("body :not(.unblur)")
+        //    .forEach(element => element.style.filter = "blur(0px)");
+    } else {
+        $('div#' + lastQuickview + '.is-active').removeClass('is-active');
+        lastQuickview = quickviewName;
+        $('div#' + quickviewName).addClass('is-active');
+        var iframe = $("#"+ quickviewName +"Iframe");
+        iframe.attr("src", iframe.data("src"));
+        //document.querySelectorAll("body :not(.unblur)")
+        //    .forEach(element => element.style.filter = "blur(2px)");
+    }
+}
+
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("content-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " is-active";
+    $('div#' + lastQuickview + '.is-active').removeClass('is-active');
+    map.invalidateSize()
+    popup.setContent("<span>Right now in<br><b>Bordeaux</b></span>");
+    marker.bindPopup(popup);
+    marker.openPopup();
+}
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 300) {
+        $('.navbar').css('background', '#12206D');
+        $('#hire-button').fadeIn();
+        $('#devname').fadeIn();
+    } else {
+        $('.navbar').css('background', 'transparent');
+        $('#hire-button').fadeOut();
+        $('#devname').fadeOut();
+    }
+});
+
